1a00653d3d69516b03d6127cd585aabf4ca3ad05
haoransun93 <haoran.sun.1993@gmail.com>
2014-09-12 16:54:03 -0400

    Update MapEditor.cpp

--- TDgame/MapEditor.cpp
+++ TDgame/MapEditor.cpp
@@ -9,3 +9,10 @@ MapEditor::MapEditor()
 MapEditor::~MapEditor()
 {
 }
+
+void MapEditor::drawGrid(Grid grid)
+{
+  // some code here that draws the grid onto the surface object, we will need to pass the surface too;
+  // this can be reused of course;
+  
+}
