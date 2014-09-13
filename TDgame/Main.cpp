c68e18a607cab87c0a33f8dff934027813088118
haoran.sun.1993@gmail.com <haoran.sun.1993@gmail.com>
2014-09-12 21:48:30 -0400

    FIRST RENDERED OBJECT
    USE KEYBOARD TO MOVE SPACESHIP

--- TDgame/Main.cpp
+++ TDgame/Main.cpp
@@ -6,21 +6,91 @@
 
 int main(int argc, char* args[])
 {
+	bool quit = false;
+	SDL_Event event;
+	int x = 288;
+	int y = 208;
 
+	// init SDL
+
+	SDL_Init(SDL_INIT_VIDEO);
+	SDL_Window * window = SDL_CreateWindow("Tower Defense",
+		SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, 0);
+	SDL_Renderer * renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+	SDL_Surface * image = SDL_LoadBMP("spaceship.bmp");
+	SDL_Texture * texture = SDL_CreateTextureFromSurface(renderer,
+		image);
+	SDL_FreeSurface(image);
+
+	SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+
+	// handle events
+
+	while (!quit)
+	{
+		SDL_WaitEvent(&event);
+
+		switch (event.type)
+		{
+		case SDL_QUIT:
+			quit = true;
+			break;
+		
+		case SDL_KEYDOWN:
+			 switch (event.key.keysym.sym)
+			 {
+			 case SDLK_LEFT:  x--; break;
+			 case SDLK_RIGHT: x++; break;
+			 case SDLK_UP:    y--; break;
+			 case SDLK_DOWN:  y++; break;
+			 }
+			 break;
+	}
+
+
+		SDL_Rect dstrect = { x, y, 64, 64 };
+
+		SDL_RenderClear(renderer);
+		SDL_RenderCopy(renderer, texture, NULL, &dstrect);
+		SDL_RenderPresent(renderer);
+	}
+
+	// cleanup SDL
+
+	SDL_DestroyTexture(texture);
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
 	// Application Window
-	SDL_Window* window = NULL; 
+	//SDL_Window* window = NULL; 
 
 	// Drawing Surface
-	SDL_Surface* screen = NULL; 
+	//SDL_Surface* screen = NULL; 
 
-	const int WIDTH = 1000;
-	const int HEIGHT = 750;
+	//const int WIDTH = 1000;
+	//const int HEIGHT = 750;
 	// Creating Window, set size
-	window = SDL_CreateWindow("Tower Defense Game", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 
-		WIDTH, HEIGHT, SDL_WINDOW_SHOWN); 
+	//window = SDL_CreateWindow("Tower Defense Game", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 
+		//WIDTH, HEIGHT, SDL_WINDOW_SHOWN); 
+
+	//creating a renderer to test
+	//SDL_Renderer * renderer = SDL_CreateRenderer(window, -1, 0);
+	//SDL_Surface * image = SDL_LoadBMP("spaceship.png");
+	//SDL_Texture * texture = SDL_CreateTextureFromSurface(renderer,		image);
+
+	//SDL_FreeSurface(image);
+
+	//SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+
+	//SDL_Rect dstrect = { 10, 10, 64, 64 };
+
+	//SDL_RenderClear(renderer);
+	//SDL_RenderCopy(renderer, texture, NULL, &dstrect);
+	//SDL_RenderPresent(renderer);
 
 	// Adding surface to window
-	screen = SDL_GetWindowSurface( window ); 
+	//screen = SDL_GetWindowSurface( window ); 
 	
 	//Check 1
 	//Will need multiple SDL_Surface objects, each for each selection of the screen (2 at least)
@@ -61,8 +131,11 @@ int main(int argc, char* args[])
 	//Please read the critter,tower,projectile, logic .h and .cpp files
 
 
-	SDL_Delay( 5000 ); 
-
+	
+	//SDL_DestroyTexture(texture);
+	//SDL_DestroyRenderer(renderer);
+	//SDL_DestroyWindow(window);
+	//SDL_Quit();
 	return 0;
 
 }
